dbfb6ac1cdd981d529f43c068f70cdcfa4534f68
Sayed Mamdouh <134965007+Shepherdeye@users.noreply.github.com>
2025-02-15 18:49:20 +0200

    Update Processing.jsx

--- src/Components/Processing.jsx
+++ src/Components/Processing.jsx
@@ -4,7 +4,7 @@ import "./Processing.css"
 
 function Processing() {
     return (
-        <div className="absolute inset-0 flex min-h-[60vh] items-center justify-center bg-gray-800 bg-opacity-75 rounded-lg">
+        <div className="absolute inset-0 flex min-h-[65vh] items-center justify-center bg-gray-800 bg-opacity-75 rounded-lg">
             <div className="loader">
                 <div className="justify-content-center jimu-primary-loading"></div>
             </div>
